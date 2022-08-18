c1d1f000f8097d27c1cf6f8369e7f384c3491803
Antonio <tonioufa@gmail.com>
2022-08-18 11:24:05 +0100

    fixes link

--- components/Song.js
+++ components/Song.js
@@ -115,6 +115,7 @@ export default function Song({ song }) {
           className="w-auto px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           href={songURL}
           target="_blank"
+          rel="noopener noreferrer"
         >
           Listen song
         </a>
